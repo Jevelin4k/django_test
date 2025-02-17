8ad86b7371f16eb806ed9c0fdd3d1951b26a2cf1
jevelin4k <jevelin4k@gmail.com>
2025-02-17 20:18:19 +0200

    3

--- level_5_projects/my_project/main_app/views.py
+++ level_5_projects/my_project/main_app/views.py
@@ -21,39 +21,42 @@ def user_logout(request):
     logout(request)
     return HttpResponseRedirect(reverse('index'))
 
+
 def register(request):
-
-    registered = False
-
-    if request.method == "POST":
-        user_form = UserForm(data=request.POST)
-        profile_form = UserProfileInfoForm(data=request.POST)
-
-        if user_form.is_valid() and profile_form.is_valid():
-
-            user = user_form.save()
-            user.set_password(user.password)
-            user.save()
-
-            profile = profile_form.save(commit=False)
-            profile.user = user
-
-            if 'profile_picture' in request.FILES:
-                profile.profile_picture = request.FILES['profile_picture']
-
-            profile.save()
-
-            registered = True
-        else:
-            print(user_form.errors, profile_form.errors)
+    if request.user.is_authenticated:
+        return HttpResponse("You are Logined")
     else:
-        user_form = UserForm()
-        profile_form = UserProfileInfoForm()
+        registered = False
 
-    return render(request, 'main_app/registration.html',
-                  {'user_form':user_form,
-                   'profile_form':profile_form,
-                   'registered':registered})
+        if request.method == "POST":
+            user_form = UserForm(data=request.POST)
+            profile_form = UserProfileInfoForm(data=request.POST)
+
+            if user_form.is_valid() and profile_form.is_valid():
+
+                user = user_form.save()
+                user.set_password(user.password)
+                user.save()
+
+                profile = profile_form.save(commit=False)
+                profile.user = user
+
+                if 'profile_picture' in request.FILES:
+                    profile.profile_picture = request.FILES['profile_picture']
+
+                profile.save()
+
+                registered = True
+            else:
+                print(user_form.errors, profile_form.errors)
+        else:
+            user_form = UserForm()
+            profile_form = UserProfileInfoForm()
+
+        return render(request, 'main_app/registration.html',
+                      {'user_form':user_form,
+                       'profile_form':profile_form,
+                       'registered':registered})
 
 def user_login(request):
 
